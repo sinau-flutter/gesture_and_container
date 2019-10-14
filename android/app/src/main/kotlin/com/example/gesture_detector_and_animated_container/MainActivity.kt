2624bdfa0cfc965291c91c37257893dc80458d7e
ayogatot <azerino25@gmail.com>
2019-10-14 16:37:59 +0700

    First Blood

---
+++ android/app/src/main/kotlin/com/example/gesture_detector_and_animated_container/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.gesture_detector_and_animated_container
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
